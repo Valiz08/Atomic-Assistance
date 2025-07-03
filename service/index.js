de2f96b57b313456cd468e5be338d7814f431128
Valiz08 <pedroval28@gmail.com>
2025-07-03 17:42:54 +0200

    test key

--- service/index.js
+++ service/index.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 app.use(cors());
 app.use(express.json());
-console.log("OPENAI_API_KEY:", process.env.OPENAI_API_KEY);
+console.log("OPENAI_API_KEY:", process.env.ko);
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
