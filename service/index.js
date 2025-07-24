3e16239abaf1071700accdd6030325f9d257b597
Valiz08 <pedroval28@gmail.com>
2025-07-24 14:13:34 +0200

    encrypt

--- service/index.js
+++ service/index.js
@@ -7,6 +7,8 @@ const app = express();
 const PORT = process.env.PORT || 3088;
 const MONGO_URI = process.env.MONGO_URI
 const client = new MongoClient(MONGO_URI);
+const bcrypt = require('bcrypt');
+
 app.use(cors());
 app.use(express.json());
 
