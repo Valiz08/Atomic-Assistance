d116f09f837c4f605ce60d96547b528f5801bae5
Valiz08 <42943804+Valiz08@users.noreply.github.com>
2026-04-21 19:27:24 +0200

    Delete service directory

--- service/models/conversation.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const ConversationSchema = new mongoose.Schema({
-  id: String,
-  number: String,
-  name: String,
-}, { strict: false });
-
-module.exports = mongoose.model('conversation', ConversationSchema, 'conversations');
